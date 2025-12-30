b92335b3baebcc2e2d7150a7a878a0928260e67e
basavesh-123 <basaveshv.cs24@bmsce.ac.in>
2025-12-30 19:09:38 +0530

    Time: 9 ms (37.62%) | Memory: 10.3 MB (65.47%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.c
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *detectCycle(struct ListNode *head) {
+    if (head == NULL || head->next == NULL)
+        return NULL;
+
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;         
+        fast = fast->next->next;    
+        
+        if (slow == fast)         
+            break;
+    }
+
+    if (fast == NULL || fast->next == NULL)
+        return NULL;
+
+    slow = head;                  
+    while (slow != fast) {
+        slow = slow->next;         
+        fast = fast->next;
+    }
+
+    return slow;
+}
